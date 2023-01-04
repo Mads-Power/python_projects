8a8935dd97dacdf51dc5a213c6e97ad257d14575
Mads-Power <mr.kolkin@gmail.com>
2023-01-04 13:04:42 +0100

    done, with backtracking algorythm

--- Sudoku/main.py
+++ Sudoku/main.py
@@ -1,4 +1,4 @@
-# Youtube credit: https://www.youtube.com/watch?v=eqUwSA0xI-s
+# Youtube credit, code along: https://www.youtube.com/watch?v=eqUwSA0xI-s
 
 board = [
     [7,8,0,4,0,0,1,2,0],
@@ -11,6 +11,48 @@ board = [
     [1,2,0,0,0,7,4,0,0],
     [0,4,9,2,0,6,0,0,7]
 ]
+
+def solve(bo):
+    find = find_empty(bo)
+    if not find:
+        return True
+    else:
+        row, col = find
+    for i in range(1,10):
+        if valid(bo, i, (row,col)):
+            bo[row][col] = i
+
+            if solve(bo):
+                return True
+
+            bo[row][col] = 0
+
+    return False
+
+
+def valid(bo,num,pos):
+        # check row
+
+        for i in range(len(bo[0])):
+            # if a number exist in position move on to next
+            if bo[pos[0]][i] == num and pos[1] != i:
+                return False
+        # check column
+        for i in range(len(bo)):
+            if bo[i][pos[1]] == num and pos[1] != i:
+                return False
+
+        #check box
+        box_x = pos[1] // 3
+        box_y = pos[0] // 3
+
+        for i in range(box_y * 3, box_y * 3 + 3):
+            for j in range(box_x * 3, box_x * 3 + 3):
+                if bo[i][j] == num and (i, j) != pos:
+                    return False
+        return True
+
+
 
 def print_board(bo):
     for i in range(len(board)):
@@ -32,3 +74,9 @@ def find_empty(bo):
         for j in range(len(bo[0])):
             if bo[i][j] == 0:
                 return (i, j) # row, col
+    return None
+
+print_board(board)
+print("after")
+solve(board)
+print_board(board)
